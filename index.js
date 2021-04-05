f34ef5c5cb93adbc85730ec15446d28d7b78a516
Akarsh Shukla <akarshshukla19@yahoo.com>
2021-04-05 00:44:06 +0000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(name, find) {
+    return name.filter(possibleMatch => 
+    possibleMatch.toLowerCase() === find.toLowerCase())
+}
+
+function fuzzyMatch(name, test) {
+    return name.filter( possibleMatch =>
+    possibleMatch.toLowerCase().indexOf(test.toLowerCase()) === 0)
+}
+
+function matchName(name, city) {
+    return name.filter(record => record.name === city)
+}
+    // Code your solution here
